36571438dba42fa218d14d10ff3cf4d3d80dc061
KareemMMarey <kareem.m.marey@gmail.com>
2020-09-19 18:59:07 +0300

    interciept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule, HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorInterceptors } from './interceptors/error.interceptors';
+import { LoadingInterceptors } from './interceptors/loading.interceptors';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptors, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptors, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptors/loading.interceptors.ts
@@ -0,0 +1,35 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router, NavigationExtras } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { catchError, delay, finalize } from 'rxjs/operators';
+import { error } from 'protractor';
+import { ToastrService } from 'ngx-toastr';
+import { BusyService } from '../services/busy.service';
+
+@Injectable()
+export class LoadingInterceptors implements HttpInterceptor{
+    constructor(private busyService: BusyService, private toastr: ToastrService)
+    {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (req.method === 'POST' && req.url.includes('orders'))
+        {
+            return next.handle(req);
+        }
+        if (req.method === 'DELETE') {
+            return next.handle(req);
+        }
+        if (req.url.includes('emailexists'))
+        {
+            return next.handle(req);
+        }
+        this.busyService.busy();
+        return next.handle(req).pipe(
+        // delay the app response to add loader
+        /* delay(1000), */
+        finalize(() => {
+            this.busyService.idle();
+        }));
+    }
+}
